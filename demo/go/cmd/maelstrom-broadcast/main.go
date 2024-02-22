ae1e459e1669d0b8dfd6da80e9d3742e6235f68c
moh ro <codemoro@outlook.com>
2024-02-23 02:23:41 +0530

    Fly.io 3b Multi node broadcast

--- demo/go/cmd/maelstrom-broadcast/main.go
+++ demo/go/cmd/maelstrom-broadcast/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -16,9 +17,71 @@ type BroadcastMessage struct {
 	Message int `json:"message,omitempty"`
 }
 
+type TopologyMessage struct {
+	// Message type
+	Type string `json:"type,omitempty"`
+
+	// Topology data
+	Topology map[string][]string `json:"topology,omitempty"`
+}
+
+type Store struct {
+	index  map[int]bool
+	cached []int
+	mu     sync.RWMutex
+}
+
+func (s *Store) InsertIfNotPresent(value int) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	_, ok := s.index[value]
+	if !ok {
+		s.index[value] = true
+	}
+	return !ok
+}
+
+func (s *Store) Read() []int {
+
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	if len(s.cached) == len(s.index) {
+		return s.cached
+	}
+
+	result := make([]int, len(s.index))
+	i := 0
+	for k := range s.index {
+		result[i] = k
+		i++
+	}
+
+	s.cached = result
+	return result
+}
+
+func Broadcast(message int, neighbours []string, node *maelstrom.Node) {
+	response := map[string]any{}
+	response["type"] = "broadcast"
+	response["message"] = message
+
+	for _, neighbour := range neighbours {
+		node.Send(neighbour, response)
+	}
+
+}
+
+func NewStore() *Store {
+	index := map[int]bool{}
+	cached := []int{}
+	return &Store{index: index, cached: cached}
+}
+
 func main() {
 	n := maelstrom.NewNode()
-	store := map[int]bool{}
+	var neighbours []string
+
+	store := NewStore()
 
 	// Register a handler for the "echo" message that responds with an "echo_ok".
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
@@ -31,7 +94,10 @@ func main() {
 		response := map[string]string{}
 		message := body.Message
 
-		store[message] = true
+		if ok := store.InsertIfNotPresent(message); ok {
+			Broadcast(message, neighbours, n)
+		}
+
 		response["type"] = "broadcast_ok"
 
 		// Update the message type.
@@ -41,6 +107,10 @@ func main() {
 		return n.Reply(msg, response)
 	})
 
+	n.Handle("broadcast_ok", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		return nil
+	})
 	n.Handle("read", func(msg maelstrom.Message) error {
 		// Unmarshal the message body as an loosely-typed map.
 		var body map[string]any
@@ -48,29 +118,22 @@ func main() {
 			return err
 		}
 
-		keys := make([]int, len(store))
-
-		i := 0
-		for k := range store {
-			keys[i] = k
-			i++
-		}
-
 		// Update the message type.
 		body["type"] = "read_ok"
-		body["messages"] = keys
+		body["messages"] = store.Read()
 
 		// Echo the original message back with the updated message type.
 		return n.Reply(msg, body)
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
-		// Unmarshal the message body as an loosely-typed map.
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
+		// Unmarshal the message message as an loosely-typed map.
+		var message TopologyMessage
+		if err := json.Unmarshal(msg.Body, &message); err != nil {
 			return err
 		}
 
+		neighbours = message.Topology[n.ID()]
 		response := map[string]string{}
 		// Update the message type.
 		response["type"] = "topology_ok"
@@ -78,6 +141,7 @@ func main() {
 		// Echo the original message back with the updated message type.
 		return n.Reply(msg, response)
 	})
+
 	// Execute the node's message loop. This will run until STDIN is closed.
 	if err := n.Run(); err != nil {
 		log.Printf("ERROR: %s", err)
